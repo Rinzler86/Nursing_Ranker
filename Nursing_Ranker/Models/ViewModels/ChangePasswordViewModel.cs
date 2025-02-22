dbf53ddd6bc4423304c068dc1d7f61443691493d
Jake-Goins <mjgoins@senators.ws.edu>
2025-02-22 15:57:37 -0500

    Mess of logging in

---
+++ Nursing_Ranker/Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,8 @@
+﻿public class ChangePasswordViewModel
+{
+    public string? CurrentPassword { get; set; }
+
+    public string? NewPassword { get; set; }
+
+    public string? ConfirmPassword { get; set; }
+}
---
+++ Nursing_Ranker/Models/ViewModels/UpdatePasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿public class UpdatePasswordViewModel
+{
+    public string? Email { get; set; }
+    public string? FavColor { get; set; }
+    public string? NewPassword { get; set; }
+    public string? ConfirmPassword { get; set; }
+    public bool IsVerified { get; set; } = false;
+}
+
+
+
+
